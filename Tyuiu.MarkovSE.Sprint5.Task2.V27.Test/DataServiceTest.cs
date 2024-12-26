31df47f05d433db6963f963b5cf0d82126c5c798
Berezkin Artem <berezkin.artjom@yandex.ru>
2024-12-26 08:29:25 +0500

    Task2

---
+++ Tyuiu.MarkovSE.Sprint5.Task2.V27.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.MarkovSE.Sprint5.Task2.V27.Lib;
+namespace Tyuiu.MarkovSE.Sprint5.Task2.V27.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckExistsFile()
+        {
+            string path = @"C:\DataSprint5\OutPutFileTask2.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = false;
+
+            Assert.AreEqual(wait, fileExists);
+
+        }
+    }
+}
