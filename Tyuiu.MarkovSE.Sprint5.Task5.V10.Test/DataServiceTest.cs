80cf6eadd79537e7f9ba91322aca9f3e10e71706
Berezkin Artem <berezkin.artjom@yandex.ru>
2024-12-26 08:49:02 +0500

    Task5

---
+++ Tyuiu.MarkovSE.Sprint5.Task5.V10.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.MarkovSE.Sprint5.Task5.V10.Lib;
+namespace Tyuiu.MarkovSE.Sprint5.Task5.V10.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestLoadFromDataFile()
+        {
+            string path = @"C:\DataSprint5\InPutDataFileTask5V10.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
