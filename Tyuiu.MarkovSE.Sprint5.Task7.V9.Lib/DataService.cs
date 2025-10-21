ab0acede67222c199fa054002cb2ecc77ae33a48
BerezkinArtem <berezkin.artem2006@gmail.com>
2025-10-21 22:20:29 +0500

    f

--- Tyuiu.MarkovSE.Sprint5.Task7.V9.Lib/DataService.cs
+++ Tyuiu.MarkovSE.Sprint5.Task7.V9.Lib/DataService.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using tyuiu.cources.programming.interfaces.Sprint5;
-using System.IO;
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
 
 namespace Tyuiu.MarkovSE.Sprint5.Task7.V9.Lib
 {
