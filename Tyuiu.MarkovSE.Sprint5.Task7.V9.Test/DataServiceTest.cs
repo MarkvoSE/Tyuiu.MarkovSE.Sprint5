80e8f97d8aa12a9c79f44b40488df06c6134da38
BerezkinArtem <berezkin.artem2006@gmail.com>
2025-10-21 22:22:23 +0500

    f

--- Tyuiu.MarkovSE.Sprint5.Task7.V9.Test/DataServiceTest.cs
+++ Tyuiu.MarkovSE.Sprint5.Task7.V9.Test/DataServiceTest.cs
@@ -2,15 +2,13 @@
 namespace Tyuiu.MarkovSE.Sprint5.Task7.V9.Test
 {
     [TestClass]
-    public sealed class DataServiceTest
+    public class DataServiceTest
     {
         [TestMethod]
         public void CheckedExistFile()
         {
-            string[] paths = { @"c:\DataSprint5", "OutPutDataFileTask7V9.txt" };
-            string fullPath = Path.Combine(paths);
-
-            FileInfo fileInfo = new FileInfo(fullPath);
+            string path = @"C:\DataSprint5\InPutDataFileTask7V9.txt";
+            FileInfo fileInfo = new FileInfo(path);
             bool fileExist = fileInfo.Exists;
             bool wait = true;
             Assert.AreEqual(wait, fileExist);
